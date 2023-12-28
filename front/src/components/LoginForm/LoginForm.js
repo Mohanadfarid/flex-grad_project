74e5d79f49c5856c510d73b0187583aa17ba424d
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2023-12-28 06:10:45 +0200

    fixed responsivness in login form

--- front/src/components/LoginForm/LoginForm.js
+++ front/src/components/LoginForm/LoginForm.js
@@ -32,7 +32,10 @@ export const LoginForm = ({
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    const res = await Apis.login_post(`https://felx-backend.onrender.com/login`, LoginData);
+    const res = await Apis.login_post(
+      `https://felx-backend.onrender.com/login`,
+      LoginData
+    );
     if (typeof res === "undefined") {
       alert(
         "there is something wrong with the back end or the ip configuration"
@@ -43,7 +46,7 @@ export const LoginForm = ({
   };
 
   const handel_tokken_and_userData = (res) => {
-    console.log(`res line 47 login form`,res);
+    console.log(`res line 47 login form`, res);
     if (res.hasOwnProperty("errors")) {
       // console.log("log in failed");
       handel_errors_state(res.errors);
@@ -77,63 +80,66 @@ export const LoginForm = ({
             : ""
         }
       />
-        <div className={styles.Form_container}>
-          <div className={styles.header_container}>
-            <i className="fa-solid fa-right-to-bracket"></i>
-            <h2>Welcome!</h2>
-            <span>Sign in to your account</span>
+      <div className={styles.Form_container}>
+        <div className={styles.header_container}>
+          <i className="fa-solid fa-right-to-bracket"></i>
+          <h2>Welcome!</h2>
+          <span>Sign in to your account</span>
+        </div>
+
+        <form onSubmit={submitHandler}>
+          <div className={styles.form_control}>
+            <label htmlFor="email">
+              Email <p className={styles.err}>{Errors.email_err}</p>{" "}
+            </label>
+            <input
+              required
+              type="email"
+              id="email"
+              name="email"
+              value={LoginData.email}
+              onChange={inputHandler}
+            />
+            <i className="fa-solid fa-user"></i>
           </div>
 
-          <form onSubmit={submitHandler}>
-            <div className={styles.form_control}>
-              <label htmlFor="email">
-                Email <p className={styles.err}>{Errors.email_err}</p>{" "}
-              </label>
-              <input
-                required
-                type="email"
-                id="email"
-                name="email"
-                value={LoginData.email}
-                onChange={inputHandler}
-              />
-              <i className="fa-solid fa-user"></i>
-            </div>
+          <div className={styles.form_control}>
+            <label htmlFor="password">
+              Password <p className={styles.err}>{Errors.password_err}</p>
+            </label>
+            <input
+              required
+              type={showPass ? "text" : "password"}
+              id="password"
+              name="password"
+              value={LoginData.password}
+              onChange={inputHandler}
+            />
+            <i
+              onClick={() => {
+                setshowPass(!showPass);
+              }}
+              className={`${styles.pass} ${
+                showPass
+                  ? "fa-sharp fa-regular fa-eye-slash"
+                  : "fa-solid fa-eye"
+              }`}
+            ></i>
+          </div>
 
-            <div className={styles.form_control}>
-              <label htmlFor="password">
-                Password <p className={styles.err}>{Errors.password_err}</p>
-              </label>
-              <input
-                required
-                type={showPass ? "text" : "password"}
-                id="password"
-                name="password"
-                value={LoginData.password}
-                onChange={inputHandler}
-              />
-              <i
-                onClick={() => {
-                  setshowPass(!showPass);
-                }}
-                className={`${styles.pass} ${
-                  showPass
-                    ? "fa-sharp fa-regular fa-eye-slash"
-                    : "fa-solid fa-eye"
-                }`}
-              ></i>
-            </div>
-
+          <div className={styles["login-reigister-container"]}>
+            <button className={styles.button} type="submit">
+              Login <i className="fa-sharp fa-solid fa-arrow-right"></i>
+            </button>
             <span className={styles.forgetpass}>
               <span>don't have an account?</span> <br />
               <Link to={"/register"}> register now!</Link>
               <br />
             </span>
-            <button className={styles.button} type="submit">
-              Login <i className="fa-sharp fa-solid fa-arrow-right"></i>
-            </button>
-          </form>
-        </div>
+          </div>
+
+        </form>
+      </div>
     </div>
   );
 };
